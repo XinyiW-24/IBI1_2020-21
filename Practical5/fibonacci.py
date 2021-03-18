147e6796685e7dff7e47c5d6c892344a8ce18240
3200110361bit <3200110361@zju.edu.cn>
2021-03-18 10:51:35 +0800

    edit.

--- Practical5/fibonacci.py
+++ Practical5/fibonacci.py
@@ -3,7 +3,7 @@ n1=1
 n2=1
 #add n1 and n2
 #give the value of n2 to n1 and give the value of n3 to n2
-#repeat this process by "for" and "i=i+1"
+#repeat this process by "for" 
 for i in range (3,14):
         n3=n1+n2
         n1=n2
