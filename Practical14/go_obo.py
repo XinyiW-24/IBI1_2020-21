b61407c32b60b0c5ea7fa838a1327cd25780c5b2
3200110361bit <3200110361@zju.edu.cn>
2021-05-18 22:04:41 +0800

    go_obo

--- Practical14/go_obo.py
+++ Practical14/go_obo.py
@@ -1,45 +1,65 @@
-#import DOM  and matplotlib
+#import DOM and matplotlib
 from xml.dom.minidom import parse
 import xml.dom.minidom
 import matplotlib.pyplot as plt
 
 DOMTree=xml.dom.minidom.parse("go_obo.xml")#get the xml file
-rootelement=DOMTree.documentElement#get the root element
-term=rootelement.getElementsByTagName("term")
+total=DOMTree.documentElement
+terms=total.getElementsByTagName('term')
 
-#get the total number of childnodes across all DNA-associated terms
-DNA=0
-for i in range(term.length):
-    if "DNA" in term[i].getElementsByTagName("defstr")[0].childNodes[0].data:#find text in defstr that contains the word DNA
-        DNA+=term[i].childNodes.length
-print('The number of childNodes associated with "DNA" is ',str(DNA))
 
-#get the total number of childnodes across all RNA-associated terms
-RNA=0
-for i in range(term.length):
-    if "RNA" in term[i].getElementsByTagName("defstr")[0].childNodes[0].data:#find text in defstr that contains the word RNA
-        RNA+=term[i].childNodes.length
-print('The number of childNodes associated with "RNA" is ',str(RNA))
+#identify id as key and is_a as value in a dictionary (one key can have more than one value)
+idis={}
+for n in terms:
+	isa=n.getElementsByTagName('is_a')
+	goid=n.getElementsByTagName('id')[0]
+	for i in isa:
+		if i.childNodes[0].data in idis:
+			idis[i.childNodes[0].data].append(goid.childNodes[0].data)#add value
+		else:
+			idis[i.childNodes[0].data]=[goid.childNodes[0].data]#add key
 
-#get the total number of childnodes across all protein-associated terms
-protein=0
-for i in range(term.length):
-    if "protein" in term[i].getElementsByTagName("defstr")[0].childNodes[0].data:#find text in defstr that contains the word protein
-        protein+=term[i].childNodes.length
-print('The number of childNodes associated with "protein" is ',str(protein))
+#use this function to find the id of the terms about one kind of molecule
+def find(molecule,Molecule,terms):
+        L=[]
+        for n in range (terms.length): #https://www.cnblogs.com/poloyy/p/12207464.html
+                if molecule in terms[n].getElementsByTagName('defstr')[0].childNodes[0].data:#find text in defstr that contains the word "molecule"
+                        L.append(terms[n].getElementsByTagName('id')[0].childNodes[0].data)
+                elif Molecule in terms[n].getElementsByTagName('defstr')[0].childNodes[0].data:##find text in defstr that contains the word "Molecule" (in case the word is in the beginning of a sentence)
+                        L.append(terms[n].getElementsByTagName('id')[0].childNodes[0].data)
+        return L
 
-#get the total number of childnodes across all glycoprotein-associated terms
-glycoprotein=0
-for i in range(term.length):
-    if "glycoprotein" in term[i].getElementsByTagName("defstr")[0].childNodes[0].data:#find text in defstr that contains the word glycoprotein
-        glycoprotein+=term[i].childNodes.length
+DNA=find('DNA','DNA',terms)
+RNA=find('RNA','RNA',terms)
+protein=find('protein','Protein',terms)
+carbohydrate=find('carbohydrate','Carbohydrate',terms)
 
-print('The number of childNodes associated with "glycoprotein" is ',str(glycoprotein))
+#use a recursion to find the childnodes of terms
+def nodenumber(idis,molelist):
+	child=[]
+	for i in molelist:
+		if i in idis:
+			childnode=idis[i]
+			child+=childnode
+			child+=nodenumber(idis,childnode)#recursion
+	return child
+
+#get the number of childnodes and use set() to avoid repetition
+DNAlength=len(set(nodenumber(idis,DNA)))
+RNAlength=len(set(nodenumber(idis,RNA)))
+proteinlength=len(set(nodenumber(idis,protein)))
+carbohydratelength=len(set(nodenumber(idis,carbohydrate)))
+
+#print out the numbers
+print('The number of childNodes of DNA-associated terms is ',str(DNAlength))
+print('The number of childNodes of RNA-associated terms is ',str(RNAlength))
+print('The number of childNodes of protein-associated terms is ',str(proteinlength))
+print('The number of childNodes of carbohydrate-associated terms is ',str(carbohydratelength))
 
 #print the pie chart
-labels='DNA','RNA','protein','glycoprotein'#define the labels
-sizes=[DNA,RNA,protein,glycoprotein]#get the sizes of each part
-explode=(0,0,0,0.1)#the fourth macromolecule is highlighted as glycoprotein is chosen by myself
+labels='DNA','RNA','protein','carbohydrate'#define the labels
+sizes=[DNAlength,RNAlength,proteinlength,carbohydratelength]#get the sizes of each part
+explode=(0,0,0,0.1)#the fourth macromolecule is highlighted as carbohydrate is chosen by myself
 plt.pie(sizes,
         explode=explode,
         labels=labels,
@@ -47,5 +67,5 @@ plt.pie(sizes,
         shadow=False,
         startangle=90)
 plt.axis('equal')
-plt.title('the pie chart of the number of childNodes associated with DNA, RNA, protein and glycoprotein')
+plt.title('the pie chart of the number of childNodes associated with DNA, RNA, protein and carbohydrate')
 plt.show()#show the pie chart
